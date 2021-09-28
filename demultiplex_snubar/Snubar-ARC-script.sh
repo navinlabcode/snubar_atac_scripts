fb667e6abe9a239190877a746e027edd1c7dcc12
Yun YAN <Puriney@users.noreply.github.com>
2021-09-28 13:37:48 -0500

    Update Snubar-ARC-script.sh

--- demultiplex_snubar/Snubar-ARC-script.sh
+++ demultiplex_snubar/Snubar-ARC-script.sh
@@ -3,6 +3,11 @@
 /cellranger-arc-1.0.0/cellranger-arc mkfastq --run=/path/to/SnubarARC-ATAC_sequencing --csv rna-sample.csv --id=fastq_gex_qc --qc --localcores=60 --localmem=600
 #---For SnubarARC-ATAC library
 #-----Cell lines
+#NOTE: For cell lines experiment, since the cellranger-arc doesn't support mixed species, we used cellrange-atac to process the scATAC data from CO-Assay experiment with the following modification:
+#--1) Replace /cellranger-atac-cs/1.2.0/lib/python/barcodes/737K-cratac-v1.txt with /cellranger_ARC/cellranger-arc-1.0.0/lib/python/atac/barcodes/737K-arc-v1.txt, but keep the original name.
+#--2) Modify line 40 of /cellranger-atac-cs/1.2.0/lib/python/constants.py from MAXIMUM_CELLS_PER_SPECIES = 20000 to MAXIMUM_CELLS_PER_SPECIES = 2000000 to previent limit warnings when overloading the cells.
+#--3) For libraries sequenced by nextseq2000, we used seqtk to remove the first 8bp spacers of cell barcode reads (usually named as R2 after demulplexing) 
+seqtk trimfq -b 8 SnubarARC-ATAC_S1_L001_R2_001.fastq.gz | gzip -c > ../SnubarARC-ATAC_S1_L001_R2_001.fastq.gz #then replace the old R2 file with the new one
 /cellranger-atac-1.2.0/cellranger-atac count --id=SnubarATAC_test --reference=/refdata-cellranger-atac-hg19-and-mm10-1.2.0 --fastqs=/Snubar_fastq/path --sample=Snuber_test1 --localcores=60 --localmem=600
 #-----Human tissues
 /cellranger-atac-1.2.0/cellranger-atac count --id=SnubarATAC_test --reference=/refdata-cellranger-atac-hg19-1.2.0 --fastqs=/Snubar_fastq/path --sample=Snuber_test1 --localcores=60 --localmem=600
